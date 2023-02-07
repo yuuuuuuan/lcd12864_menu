b1cd4e5006288ef6a227712a61396e3154f52125
yuuuuuuan <99110203+yuuuuuuan@users.noreply.github.com>
2023-02-07 17:59:18 +0800

    Add files via upload

---
+++ codes/main.c
@@ -0,0 +1,17 @@
+#include 		"STC15F.h"
+#include    "intrins.h"
+#include    "GPIO.H"
+#include    "KeyScan.h"
+#include    "LCD12864.h"
+#include 		"Delay.h"
+void GPIO();
+
+u8  code open_line1[] = {" GAME"};
+u8  code open_line2[] = {" Push the Boxes"};
+u8  code open_line3[] = {" ENTER   QUIT "};
+u8  code open_line4[] = {" MadeByYuuuan"};
+
+void TOPIC(void)
+{
+	
+}
